e86b12f4395c03fb81bd0585538204b324660d58
Muhammed Sahin <muhammed.sahin@acp.at>
2020-08-01 03:05:23 +0200

    Started the Project # added the Components

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,27 @@
+import React from "react";
+import classes from "./Burger.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
+const burger = (props) => {
+  let tranformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (tranformedIngredients.length === 0) {
+    tranformedIngredients = <p>Please start adding Ingredients</p>;
+  }
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {tranformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default burger;
