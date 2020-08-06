c77fe12162b5f5c26f1f9d5240d2c2f221541d26
meminsahin <m.emins@yahoo.com>
2020-08-06 20:00:00 +0200

    starting with redux

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://burgerbuilder-react-57c56.firebaseio.com",
+  baseURL: "https://burgerbuilder-react-57c56.firebaseio.com/",
 });
 
 export default instance;
